ea07afa8731d99f97b1c63960fd60f5c14d3cddd
Veparkumar <108547243+Veparkumar@users.noreply.github.com>
2024-07-03 21:30:05 +0530

    Create pizza.py

---
+++ pizza.py
@@ -0,0 +1,22 @@
+print("Thank you for choosing pizza deliverlies!")
+size = input()
+add_pepperoni = input()
+extra_cheese = input()
+bill = 0
+if size == "S":
+  bill += 15
+elif size == "M":
+  bill += 20
+else:
+  size == "L"
+  bill += 25
+
+if add_pepperoni == "Y":
+  if size == "S":
+    bill += 2
+else:
+  bill += 3
+
+if extra_cheese == "Y":
+  bill += 1
+print(f"Your final bill is: {bill}")
